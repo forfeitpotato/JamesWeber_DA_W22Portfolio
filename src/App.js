b146a3d62b78cbad4255907d499bb5eb15bd9949
forfeitpotato <74336613+forfeitpotato@users.noreply.github.com>
2022-03-16 16:55:44 -0400

    further testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>This is a test</div>
+    <div>
+      <h1>Forfeit Potato</h1>
+      <p>Link to resume or write resume</p>
+      <p>Link to cover letter</p>
+      <p>Short philosophy</p>
+      <p>Academic credentials (Unlikely)</p>
+      <h2>Section for academic work</h2>
+      <p>Maybe 5 links to work</p>
+      <p>Talk about capstone project</p>
+      <p>Professional summary (work experience maybe some personal projects)</p>
+    </div>
+
   );
 }
 
